b0b8357fb733d317dea50c765bb5c9aeecd40257
wangyupu <wangyupu4513@gmail.com>
2025-09-26 23:16:54 +0800

    优化fmt示例

--- src/scripts/fmt_example/Main.hx
+++ src/scripts/fmt_example/Main.hx
@@ -4,14 +4,18 @@ import cc_basics.Base;
 import fmt.Manager;
 
 class Main {
+	private static var variable:Int = 0;
+
 	private static function task1() {
 		Base.sleep(3);
-		Base.print("Task 1 done", Base.clock());
+		Base.print("Task 1 done - ", Base.clock());
+		variable *= 2;
 	}
 
 	private static function task2() {
 		Base.sleep(5);
-		Base.print("Task 2 done", Base.clock());
+		Base.print("Task 2 done - ", Base.clock());
+		variable *= 3;
 	}
 
 	public static function main() {
@@ -20,8 +24,9 @@ class Main {
 		ThreadManager.add(task2);
 		ThreadManager.start();
 		while (true) {
-			Base.print("Main - doing something");
+			Base.print("Main - doing something - ", Base.clock(), " - ", variable);
 			Base.sleep(1);
+			variable++;
 		}
 	}
 }
